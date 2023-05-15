f36ad3b46ddbd6005e7db2f10a939e6033d34fa9
ɪᴄᴄ ᴛᴇᴀᴍ <131968962+ICC-TG-BOT@users.noreply.github.com>
2023-05-15 08:50:16 +0530

    start.py

--- VipX/utils/inline/start.py
+++ VipX/utils/inline/start.py
@@ -64,7 +64,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         [
             InlineKeyboardButton(
                 text="🌱ѕσʋяcɛ🌱",
-                url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
+                url=f"https://github.com/ICC-TG-BOT/BOXER",
             )
         ],
      ]
