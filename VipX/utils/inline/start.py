76f715bb0ea6afb0e392b5199c8d6cdc23851128
ɪᴄᴄ ᴛᴇᴀᴍ <131968962+ICC-TG-BOT@users.noreply.github.com>
2023-06-09 14:19:44 +0530

    Update start.py

--- VipX/utils/inline/start.py
+++ VipX/utils/inline/start.py
@@ -20,17 +20,17 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🥵 𝐀𝐃𝐃 𝐌𝐄 𝐁𝐀𝐁𝐔 🥵",
+                text="✰ 𝐀𝐃𝐃 𝐌𝐄 𝐁𝐀𝐁𝐔 ✰",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🤩𝐅𝐄𝐀𝐓𝐔𝐑𝐄🤩",
+                text="✰ 𝐅𝐄𝐀𝐓𝐔𝐑𝐄 ✰",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="⚙️𝐒𝐄𝐓𝐓𝐈𝐍𝐆⚙️", callback_data="settings_helper"
+                text="✰ 𝐒𝐄𝐓𝐓𝐈𝐍𝐆 ✰", callback_data="settings_helper"
             ),
         ],
      ]
@@ -41,29 +41,29 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🥵 𝐀𝐃𝐃 𝐌𝐄 𝐅𝐀𝐒𝐓 𝐁𝐀𝐁𝐔 🥵",
+                text="✰ 𝐀𝐃𝐃 𝐌𝐄 𝐅𝐀𝐒𝐓 𝐁𝐀𝐁𝐔 ✰",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="😎 𝐎𝐖𝐍𝐄𝐑 😎", url=f"https://t.me/{OWNER_USERNAME}",
+                text="✰ 𝐎𝐖𝐍𝐄𝐑 ✰", url=f"https://t.me/{OWNER_USERNAME}",
             ),
             InlineKeyboardButton(
-                text="😣 𝐇𝐄𝐋𝐏 😣", callback_data="settings_back_helper"
+                text="✰ 𝐇𝐄𝐋𝐏 ✰", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="🙈𝐒𝐔𝐏𝐏𝐎𝐑𝐓🙈", url=f"https://t.me/{YOUR_GROUP}",
+                text="✰ 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 ✰", url=f"https://t.me/{YOUR_GROUP}",
             ),
             InlineKeyboardButton(
-                text="🍁𝐔𝐏𝐃𝐀𝐓𝐄𝐒🍁", url=f"https://t.me/{YOUR_CHANNEL}",
+                text="✰ 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 ✰", url=f"https://t.me/{YOUR_CHANNEL}",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌷ѕσʋяcɛ🌷", 
+                text="✰ ѕσʋяcɛ ✰", 
               
                 url=f"https://te.legra.ph/file/94ac7b6ffc9e8b8f8cce0.mp4",
             )
